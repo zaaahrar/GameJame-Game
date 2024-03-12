824dfa844ea4144b76b2e5d223103aa332140f4d
zaaahrar <zaahrar@mail.ru>
2024-03-12 06:20:39 +0300

    Finish2.0

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -4,21 +4,22 @@ using UnityEngine.SceneManagement;
 public class Finish : MonoBehaviour
 {
     [SerializeField] private CounterPortal _counter;
+    [SerializeField] private bool _isBlue;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out Player player))
         {
+            if (_isBlue)
+                _counter.AddBlue();
+            else
+                _counter.AddRed();
+
+
             if(_counter.CountBlue > _counter.CountRed)
-            {
                 SceneManager.LoadScene(9);
-                Debug.Log("ss");
-            }
             else if(_counter.CountRed > _counter.CountBlue)
-            {
                 SceneManager.LoadScene(10);
-                Debug.Log("ssss");
-            }
         }
     }
 }
